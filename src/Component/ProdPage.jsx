0dea3e9d79c384e5be257c1e902fb618611add43
Ihon <onosetaleoseghale@gmail.com>
2022-02-25 22:52:47 +0100

    Total price by count LOGIC

--- src/Component/ProdPage.jsx
+++ src/Component/ProdPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {useState} from "react";
-import {makeStyles} from "@mui/styles";
+import { useState } from "react";
+import { makeStyles } from "@mui/styles";
 import Sneakers from "../assets/images/image-product-1.jpg";
 import Sneakers1 from "../assets/images/image-product-1-thumbnail.jpg";
 import Sneakers2 from "../assets/images/image-product-2-thumbnail.jpg";
@@ -10,27 +10,27 @@ import { Typography } from "@mui/material";
 import { Image, Row, Col } from 'antd';
 import "../App";
 
-const useStyles = makeStyles((theme)=>({
-    sneaker:{
-        display: "grid",    
+const useStyles = makeStyles((theme) => ({
+    sneaker: {
+        display: "grid",
         gridTemplateColumns: "repeat(3, 1fr)",
         gap: "10px",
-        gridAutoRows: "100px",   
-        margin: "13rem", 
+        gridAutoRows: "100px",
+        margin: "13rem",
         alignItems: "center",
     },
-    Shoe1:{
+    Shoe1: {
         /*display: "inline-block",*/
-        justifySelf:"center",
+        justifySelf: "center",
     },
-    Shoe2:{
-        justifySelf:"end",
+    Shoe2: {
+        justifySelf: "end",
     },
-  
+
 
 }));
 
-const ProductPage=()=>{
+const ProductPage = () => {
     const classes = useStyles();
     const [visible, setVisible] = useState(false);
     const [count, setCount] = useState(0);
@@ -38,74 +38,80 @@ const ProductPage=()=>{
 
     const price = 125;
 
-    return(
-       <React.Fragment>
-           <div className={classes.sneaker}>
-               <div className={classes.Shoe1}>  
-               <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                   <Col className="gutter-row" span={8}>
-                   <Image
-                           preview={{ visible: false }}
-                           width={265}
-                           height={250}
-                           borderRadius={10}
-                           src={Sneakers}
-                           onClick={() => setVisible(true)}
-                         />
-                         <div style={{ display: 'flex' }}>
-                           <Image.PreviewGroup preview={{ visible, onVisibleChange: vis => setVisible(vis) }}>
-                             <Image src={Sneakers1} />
-                             <Image src={Sneakers2} />
-                             <Image src={Sneakers3} />
-                             <Image src={Sneakers4} />
-                           </Image.PreviewGroup>
-                         </div>
+    return (
+        <React.Fragment>
+            <div className={classes.sneaker}>
+                <div className={classes.Shoe1}>
+                    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                        <Col className="gutter-row" span={8}>
+                            <Image
+                                preview={{ visible: false }}
+                                width={265}
+                                height={250}
+                                borderRadius={10}
+                                src={Sneakers}
+                                onClick={() => setVisible(true)}
+                            />
+                            <div style={{ display: 'flex' }}>
+                                <Image.PreviewGroup preview={{ visible, onVisibleChange: vis => setVisible(vis) }}>
+                                    <Image src={Sneakers1} />
+                                    <Image src={Sneakers2} />
+                                    <Image src={Sneakers3} />
+                                    <Image src={Sneakers4} />
+                                </Image.PreviewGroup>
+                            </div>
 
 
-                   </Col>
-                   </Row>                            
-                        
+                        </Col>
+                    </Row>
+
                 </div>
-               <div className={classes.Shoe2}>
-                   <h5
-                     style={{color: "hsl(26, 100%, 55%)", 
-                          fontWeight: "400", 
-                          fontStyle:"Bold"}}>
-                          SNEAKER COMPANY
-                   </h5>
-                   <Typography
-                   variant="h1" style={{fontWeight: "700", fontSize: "24px", lineHeight: "1.5em"}}>
-                       Fall Limited Edition <br />
-                       Sneakers
-                   </Typography>
-                   <small style={{lineHeight: "1.5em"}}>These low profile sneakers are your perfect casual wear
-                       <br />companion. Featuring a durable rubber outer sole, they'll
-                       <br />withstand everything the whether as to offer.
-                   </small><br />
-                   <small><strong>$125.00</strong></small><br />
-                   <div className="button">
-                     
-                     <p>
-                     <button onClick={() => setCount(count - 1)} className="remove">-</button>
-                     {count} 
-                     <button onClick={() => setCount(count + 1)} className="add" >+</button>
-                    </p>
-                     
-                     <p>
-                     <button className="cart"
-                      onClick={()=> setCount(count*125)}
-                     >
-                         <img src="
-                          ./assets/images/icon-cart.svg);" alt=""/>
-                          Add to Cart
-                        </button>
+                <div className={classes.Shoe2}>
+                    <h5
+                        style={{
+                            color: "hsl(26, 100%, 55%)",
+                            fontWeight: "400",
+                            fontStyle: "Bold"
+                        }}>
+                        SNEAKER COMPANY
+                    </h5>
+                    <Typography
+                        variant="h1" style={{ fontWeight: "700", fontSize: "24px", lineHeight: "1.5em" }}>
+                        Fall Limited Edition <br />
+                        Sneakers
+                    </Typography>
+                    <small style={{ lineHeight: "1.5em" }}>These low profile sneakers are your perfect casual wear
+                        <br />companion. Featuring a durable rubber outer sole, they'll
+                        <br />withstand everything the whether as to offer.
+                    </small><br />
+                    <small><strong>$125.00</strong></small><br />
+                    <div className="button">
+                        <p>
+                            <button onClick={() => setCount(count - 1)} className="remove">-</button>
+                            {count}
+                            <button onClick={() => setCount(count + 1)} className="add" >+</button>
                         </p>
-                   </div>
 
-               </div>
 
-           </div>
-       </React.Fragment>
+                        {/* total price  */}
+                        <h4>Total by quantity</h4>
+                        <h1>$ {count * 125} </h1>
+
+                        <p>
+                            <button className="cart"
+                                onClick={() => setCount(count * 125)}
+                            >
+                                <img src="
+                          ./assets/images/icon-cart.svg);" alt="" />
+                                Add to Cart
+                            </button>
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+        </React.Fragment>
     );
 }
 
